c8b702f38be13f3bb0a5b1e9b24944d0f71c8dc5
yuveex <alvinsetiawan2003@gmail.com>
2024-02-22 14:29:27 +0700

    Sign Up Comment

--- app/src/main/java/com/ubaya/anmp_week1/SignUp.kt
+++ app/src/main/java/com/ubaya/anmp_week1/SignUp.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class SignUp : AppCompatActivity() {
+
+    // Sign Up Comment Here
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
