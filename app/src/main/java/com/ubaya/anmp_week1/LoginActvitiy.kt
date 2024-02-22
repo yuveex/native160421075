18429d7e21dfe9c1c3ac98e81a2cfef5e5d81d64
yuveex <alvinsetiawan2003@gmail.com>
2024-02-22 14:09:46 +0700

    Added Login Activity :D

---
+++ app/src/main/java/com/ubaya/anmp_week1/LoginActvitiy.kt
@@ -0,0 +1,11 @@
+package com.ubaya.anmp_week1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActvitiy : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_actvitiy)
+    }
+}
