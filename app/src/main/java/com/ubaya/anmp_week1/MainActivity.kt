02a1c3007f56333fe1e72380b64bff937180e604
yuveex <alvinsetiawan2003@gmail.com>
2024-02-22 13:55:12 +0700

    Added Main Activity :>

---
+++ app/src/main/java/com/ubaya/anmp_week1/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ubaya.anmp_week1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
